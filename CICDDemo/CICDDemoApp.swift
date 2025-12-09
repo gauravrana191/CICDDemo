132dd22d223370fd5b90a838f84e9dd7b199d69a
Gaurav Kumar <gauravrana191@gmail.com>
2025-12-09 13:40:24 +0530

    Initial Commit

---
+++ CICDDemo/CICDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDDemoApp.swift
+//  CICDDemo
+//
+//  Created by Gaurav Kumar on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CICDDemo
+//
+//  Created by Gaurav Kumar on 09/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Helloooo, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
